4979cea8de5f6328c018d9bdb638c54700a9f6ac
branzbbest <rattaphoom_eikumnerd@hotmail.com>
2020-08-11 17:50:49 +0700

    add news_1 route

--- start/routes.js
+++ start/routes.js
@@ -18,8 +18,9 @@ const AuthController = require('../app/Controllers/Http/AuthController');
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.get("/home" , "AuthController.home")
+Route.get("/news_1" , "AuthController.news")
 
+Route.get("/home" , "AuthController.home")
 //Route.on('/login').render('login')
 
 Route.get("/login" , "AuthController.login")
@@ -29,7 +30,7 @@ Route.get("/layout" , "AuthController.logoutUser")
 
 Route.get("/register" , "AuthController.register")
 Route.post("/register" , "AuthController.registerUser")
-Route.post("/api/register" , "AuthController.registerUser")
+Route.get("/api/register" , "AuthController.registerUser")
 
 Route.get("/index" , "AuthController.index")
 Route.get("/slideshow" , "AuthController.slideshow")
